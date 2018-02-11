8bfd004898a5cf85e43afc0ad1cf88b5aaf5dfcd
ankesh007 <ankeshgupta007@gmail.com>
2018-02-11 17:54:03 +0530

    Added complete LWLR code

--- Assignment 1/LWLR.py
+++ Assignment 1/LWLR.py
@@ -2,6 +2,8 @@ from Regression import Regression
 import sys
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 class LocallyWeightedLinearRegression(Regression):
 
@@ -28,7 +30,7 @@ class LocallyWeightedLinearRegression(Regression):
 
 		[instances,parameters]=self.x.shape
 
-		temp_x=x
+		temp_x=np.copy(x)
 		if(self.has_normalized_x==True):
 			temp_x=self.normalize(temp_x,self.x_mu,self.x_sigma)
 
@@ -38,6 +40,7 @@ class LocallyWeightedLinearRegression(Regression):
 		norm_x=np.square(norm_x)
 		norm_x=norm_x/(2*tau*tau)
 		weight=np.exp(-1*norm_x)
+		# print(weight)
 		weight=np.diag(weight[:,0])
 		# print(weight.shape)
 
@@ -48,7 +51,24 @@ class LocallyWeightedLinearRegression(Regression):
 
 		return temp_eval
 
+def plotPoints_Hypothesis(myReg,prediction,filename):
 
+	plt.figure(1)
+	fig, ax = plt.subplots()
+	plt.xlabel("Normalized X")
+	plt.ylabel("Normalized Y")
+	plt.title("LWLR- Data and Hypothesis")
+	# print(myReg.unappended_x[(myReg.y[:,0]==1)])
+	argsort_val=myReg.unappended_x[:,0].argsort()
+	ax.plot(myReg.unappended_x,myReg.y,'ro',label='data')
+	# print(myReg.unappended_x[argsort_val].shape)
+	# print(argsort_val)
+	# print(myReg.unappended_x[argsort_val][:,:,0])
+	ax.plot(myReg.unappended_x[argsort_val],prediction[argsort_val],'k',label='hypothesis')
+
+	legend = ax.legend(loc='upper left',fontsize='x-small')
+	legend.get_frame().set_facecolor('#00FFCC')
+	plt.savefig(filename+'.png')
 
 
 def main(path_x,path_y):
@@ -57,7 +77,20 @@ def main(path_x,path_y):
 	temp_y=pd.read_csv(path_y,header=None,sep=',')
 
 	myReg=LocallyWeightedLinearRegression(pd_x=temp_x,pd_y=temp_y)
-	myReg.predict(x=temp_x.values[0:1,0:1],tau=0.2)
+
+	temp_x_val=temp_x.values
+	[instances,_]=temp_x_val.shape
+	print (instances)
+	prediction=np.zeros([0,1])
+
+	for i in range(instances):
+		query_x=myReg.predict(temp_x_val[i:i+1],100)
+		prediction=np.concatenate((prediction,query_x),axis=0)
+		# print(query_x)
+
+	plotPoints_Hypothesis(myReg,prediction,'LWLR')
+	# print(np.concatenate((prediction,temp_y.values),axis=1))
+	# print(myReg.predict(x=temp_x.values[2:3,0:1],tau=0.2),myReg.y[2])
 	# myReg.solveAnalytically()
 	# myReg.train(log_every_epoch=50,learning_rate=0.01,epsilon=0.0001)
 	# print (myReg.train_steps)
