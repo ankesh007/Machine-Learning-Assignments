874982d930a93170c8e28f6ded386e496534010f
ankesh007 <ankeshgupta007@gmail.com>
2018-04-04 18:58:57 +0530

    Added DecisionTree notebook

--- Assignment 3/read_data.py
+++ Assignment 3/read_data.py
@@ -55,7 +55,7 @@ def medians(file):
     fin.close()
     return(statistics.median(age),statistics.median(fnlwgt),statistics.median(edun),statistics.median(capg),statistics.median(capl),statistics.median(hpw))
 
-def preprocess(file):
+def preprocess(file,binarise=True):
     """
     Given a file, read its data by encoding categorical attributes and binarising continuos attributes based on median.
     params(1): 
@@ -64,7 +64,8 @@ def preprocess(file):
         2D numpy array with the data
     """
     # Calculate the medians
-    agem,fnlwgtm,edunm,capgm,caplm,hpwm = medians(file)
+    if(binarise):
+    	agem,fnlwgtm,edunm,capgm,caplm,hpwm = medians(file)
     fin = open(file,"r")
     reader = csv.reader(fin)
     data = []
@@ -84,12 +85,18 @@ def preprocess(file):
         
         # Binarize the numerical attributes based on median.
         # Modify this section to read the file in part c where you split the continuos attributes baed on dynamic median values.
-        t[1] = float(l[0])>=agem; t[3] = float(l[2])>=fnlwgtm; t[5] = float(l[4])>=edunm;
-        t[11] = float(l[10])>=capgm; t[12] = float(l[11])>=caplm; t[13] = float(l[12])>=hpwm;
+        if(binarise):
+        	t[1] = float(l[0])>=agem; t[3] = float(l[2])>=fnlwgtm; t[5] = float(l[4])>=edunm;
+        	t[11] = float(l[10])>=capgm; t[12] = float(l[11])>=caplm; t[13] = float(l[12])>=hpwm;
         
+        else:
+        	t[1] = float(l[0]); t[3] = float(l[2]); t[5] = float(l[4]);
+        	t[11] = float(l[10]); t[12] = float(l[11]); t[13] = float(l[12]);
+        
+
         # Convert some of the booleans to ints
         data.append([int(x) for x in t])
-    return np.array(data,dtype=np.int64)
+    return np.array(data,dtype=np.float32)
 
 ## Read the data
 # train_data = preprocess("train.csv")
